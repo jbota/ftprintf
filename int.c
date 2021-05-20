9774abe9792985dd8a9c0f28696586ac346d8fe5
jbota <jbota@DESKTOP-MMGJA8T.localdomain>
2021-05-20 12:47:07 +0100

    ola

--- int.c
+++ int.c
@@ -16,14 +16,9 @@ int	signzerowidth(t_struct *list, char c, char sinal, int sign)
 {
 	if (list->width > 0 && c == '0')
 	{
-		if (list->space)
-		{
-			list->nprinted += write(1, " ", 1);
-			list->space = 0;
-		}
 		if (sinal != '\0')
 			list->nprinted += write(1, &sinal, 1);
-		sign -= sign;	
+		sign = -sign;
 	}
 	return (sign);
 }
@@ -33,43 +28,40 @@ void	right_align(t_struct *list, int len, char *s, int sign)
 	char	sinal;
 	char	c;
 
+//	sign = signzerowidth(list, c, sinal, sign);
 	if (list->zero && !list->dot)
 		c = '0';
 	else
 		c = ' ';
 	sinal = '\0';
-	if (sign == '+')
-		sinal = '+';
-	else if (sign == '-')
+	if (sign == '-')
 		sinal = '-';
 	sign = signzerowidth(list, c, sinal, sign);
 	while (list->width-- > 0)
+	{
 		list->nprinted += write(1, &c, 1);
-	if (list->space != 0)
-		list->nprinted += write(1, " ", 1);
+	}
 	if (sign != 0 && sinal != '\0')
-		list->nprinted += write(1, &sinal, 1);
-	list->nprinted += write(1, s, len);
+		list->nprinted += write(1, &sign, 1);
+	while (list->precision-- > 0)
+		list->nprinted += write(1, "0", 1);
+	write(1, s, len);
+	list->nprinted += len;
 }
 
 void	left_align(t_struct *list, int len, char *s, int sign)
 {
-	if (list->space)
-		list->nprinted += write(1, " ", 1);
-	if (sign == '+')
-		list->nprinted += write(1, "+", 1);
-	else if (sign == '-')
+	if (sign == '-')
 		list->nprinted += write(1, "-", 1);
-	while (list->precision > 0)
+	while (list->precision-- > 0)
 	{
 		list->nprinted += write(1, "0", 1);
-		list->precision--;
 	}
-	list->nprinted += write(1, s, len);
-	while (list->width > 0)
+	write(1, s, len);
+	list->nprinted += len;
+	while (list->width-- > 0)
 	{
 		list->nprinted += write(1, " ", 1);
-		list->width--;
 	}
 }
 
@@ -78,8 +70,6 @@ void	formatint(int n, t_struct *list, int sign)
 	int	len;
 	char	*s;
 
-	if (list->space && sign != 0)
-		list->space = 0;
 	s = ft_itoa(n);
 	len = manel(n);
 	if (n == 0)
@@ -91,9 +81,11 @@ void	formatint(int n, t_struct *list, int sign)
 	else
 		list->precision = 0;
 	if (sign != 0)
-		list->width = list->width - (list->precision + len + 1 + list->space);
+		list->width = list->width - (list->precision + len + 1);
 	else
-		list->width = list->width - (list->precision + len + list->space);
+		list->width = list->width - (list->precision + len);
+	if (list->width < 0)
+		list->width = 0;
 	if (list->minus == 0)
 		right_align(list, len, s, sign);
 	if (list->minus == 1)
@@ -104,9 +96,7 @@ void	formatint(int n, t_struct *list, int sign)
 void	isint(t_struct *list, va_list args, int sign, int i)
 {
 	i = (int)va_arg(args, int);
-	if (list->plus && i >= 0)
-		sign = '+';
-	else if (i < 0)
+	if (i < 0)
 	{
 		sign = '-';
 		i = i * -1;
